a18a1a353d3fbe1a210ddb29dbf5a8ec70778a41
Mateusz <mati@MB-Mati.local>
2023-05-26 16:45:00 +0200

    UserModel.cs

---
+++ Formula1/Models/UserModel.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Formula1.Models
+{
+    public class UserModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
